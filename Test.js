e56be61ecb651167a23d6594734f4f336dd07bc2
Raizaen <31708305+Raizaen@users.noreply.github.com>
2018-04-16 17:20:42 +0200

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,11 @@
+//var 
+var nom = ...; //type automatique
+
+//dom
+var balId = window.document.getElementById("JeanJacques"); // stockage de la balise avec l'id jeanjacques 
+var tabBalClass = window.document.getElementByClassName("Rousseau"); // stock tab contient toutes les balises qui ont class Rousseau
+
+var newBal = window.document.createElement("div"); // crée balise div 
+
+window.document.removeChild(elementDOM); // Remove an HTML element
+body.appendChild(elementDOM); // Add an HTML element
